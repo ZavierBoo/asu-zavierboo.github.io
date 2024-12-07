6436fc31065514c0b63dfcdca9a7225e78499962
Zavier <zabooker@asu.edu>
2024-12-06 21:08:45 -0700

    Add new portfolio files

---
+++ final/script2.js
@@ -0,0 +1,5 @@
+// Script for interactivity on the portfolio page
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! I'll get back to you soon.");
+});
